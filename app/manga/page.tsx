1b869125831632b2c4b29e10157ccffe41b05dd1
XerAnime <160598198+XerAnime@users.noreply.github.com>
2024-05-26 14:33:24 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ app/manga/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+export default function Home() {
+  return (
+    <>
+      <main className="flex flex-col items-center justify-between p-24">
+        <div className="flex justify-center mb-4 mt-8">
+          <h1 className="text-3xl font-mono leading-tight tracking-tighter md:text-4xl">
+            COMING SOON
+          </h1>
+        </div>
+      </main>
+    </>
+  );
+}
